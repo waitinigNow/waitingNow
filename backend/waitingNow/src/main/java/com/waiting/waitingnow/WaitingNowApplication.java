58025eeaab0da02be8af6c7c859b668a0cea84a7
전현준 <31847789+guswns7452@users.noreply.github.com>
2023-11-07 00:51:02 +0900

    feat: git init & 프로젝트 초기 세팅
    git init과 backend와 frontend 폴더로 구분하여
    프로젝트 세팅을 마쳤습니다.

---
+++ backend/waitingNow/src/main/java/com/waiting/waitingnow/WaitingNowApplication.java
@@ -0,0 +1,13 @@
+package com.waiting.waitingnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaitingNowApplication.class, args);
+    }
+
+}
---
+++ backend/waitingNow/src/test/java/com/waiting/waitingnow/WaitingNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waiting.waitingnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

e8bcb1757b2c6dfdd3ade54969aa9319e569a90e
전현준 <31847789+guswns7452@users.noreply.github.com>
2023-11-08 01:48:39 +0900

    feat: menu Option VO, Mapper 개발
    menu_option 관련한 VO와 Mapper 개발 완료

---
+++ backend/waitingNow/src/main/java/com/waiting/waitingnow/domain/optionMenuVO.java
@@ -0,0 +1,40 @@
+package com.waiting.waitingnow.domain;
+
+public class optionMenuVO {
+    private int optionMenuKey;
+    private int menuNumber;
+    private String optionMenu_Name;
+    private int optionMenuPrice;
+
+    public int getOptionMenuKey() {
+        return optionMenuKey;
+    }
+
+    public void setOptionMenuKey(int optionMenuKey) {
+        this.optionMenuKey = optionMenuKey;
+    }
+
+    public int getMenuNumber() {
+        return menuNumber;
+    }
+
+    public void setMenuNumber(int menuNumber) {
+        this.menuNumber = menuNumber;
+    }
+
+    public String getOptionMenu_Name() {
+        return optionMenu_Name;
+    }
+
+    public void setOptionMenu_Name(String optionMenu_Name) {
+        this.optionMenu_Name = optionMenu_Name;
+    }
+
+    public int getOptionMenuPrice() {
+        return optionMenuPrice;
+    }
+
+    public void setOptionMenuPrice(int optionMenuPrice) {
+        this.optionMenuPrice = optionMenuPrice;
+    }
+}

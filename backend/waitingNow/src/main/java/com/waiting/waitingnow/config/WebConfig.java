2b5d206d26b58d20068919e3c36ef4f20a0cb32d
전현준 <31847789+guswns7452@users.noreply.github.com>
2023-12-02 02:56:42 +0900

    refactor: package 위치 수정

--- backend/waitingNow/src/main/java/com/waiting/waitingnow/config/WebConfig.java
+++ backend/waitingNow/src/main/java/com/waiting/waitingnow/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.waiting.waitingnow;
+package com.waiting.waitingnow.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
